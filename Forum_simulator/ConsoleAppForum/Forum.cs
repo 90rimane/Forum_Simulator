e984cf277df73bef06da86e7601e1d1546cc7e91
ali <rima.aneta@gmail.com>
2020-12-17 20:12:42 +0100

    Completering console app

--- Forum_simulator/ConsoleAppForum/Forum.cs
+++ Forum_simulator/ConsoleAppForum/Forum.cs
@@ -7,13 +7,128 @@ namespace ConsoleAppForum
 {
     public class Forum
     {
-        private Post _post = new Post();
-        private User _user = new User();
-        private Thread _thread = new Thread();
+        private Post _postRepo = new Post();
+        private User _userRepo = new User();
+        private Thread _threadRepo = new Thread();
 
         public void Run()
         {
+            //PrintThreads(_threadRepo);
+            //PrintThreadContentById(_postRepo,_threadRepo);
+            //ListUsers(_userRepo);
+            //AddThread();
+            //AddPost();
+            //UpdatePost(_postRepo);
+            //DeletePost(_postRepo);
 
+        }
+        public void PrintThreads(Thread repo)
+        {
+            var threads = repo.GetThreads();
+            Console.WriteLine("\t------Threads------");
+            Console.WriteLine("Id\t | Subject\t | User Name | Post Count\n");
+            foreach (var thread in threads)
+            {
+                if (thread.PostCount == 0)
+                {
+                    Console.WriteLine($"{thread.Id}\t | {thread.Subject}\t\t | {thread.PostCount}");
+                }
+                else
+                {
+                    Console.WriteLine($"{thread.Id}\t | {thread.Subject}\t | {thread.Creator}\t | {thread.PostCount}");
+                }
+            }
+        }
+        public void PrintThreadContentById(Post postrepo, Thread threadrepo)
+        {
+            Thread repo = _threadRepo;
+            var threads = repo.GetThreads();
+            Console.WriteLine("\t------List Threads Content By Id------\n");
+
+            foreach (var thre in threads)
+                Console.Write($"|{thre.Id}-{thre.Subject} |");
+
+            Console.Write("\n\nEnter Thread Id: ");
+
+            var threadId = int.Parse(Console.ReadLine());
+            var thread = threadrepo.GetById(threadId);
+            var posts = postrepo.GetPostsByThread(thread);
+
+            Console.WriteLine($"\n\t------\"{thread.Subject}\"------\n" +
+                              $"\nUser | Post (Post Id)\n");
+
+            foreach (var p in posts)            
+                Console.WriteLine($"\t{p.User.UserName} | {p.Content} ({p.Id})\n");            
+        }
+        public void ListUsers(User repo)
+        {
+            var users = repo.GetUsers();
+            Console.WriteLine("\t------Users List------");
+            Console.WriteLine("\nid| User Name\n");
+            foreach (var u in users)
+                Console.WriteLine($"{u.Id}| {u.UserName}");
+        }
+        public void AddThread()
+        {
+            var thread = new Thread();
+            Console.WriteLine("\t------Create Thread------\n");
+            Console.Write("Enter a Subject : ");
+            thread.Subject = Console.ReadLine();
+            _threadRepo.CreateThread(thread);
+            Console.WriteLine("\nThreat Created. Press Any Key To Continue...");
+            Console.ReadKey();
+        }
+        public void AddPost()
+        {
+            var post = new Post();
+            Console.WriteLine("\t------Create Post------\n");
+
+            User repo = _userRepo;
+            var users = repo.GetUsers();
+            foreach (var u in users)
+            {
+                Console.Write($"{u.Id}|{u.UserName} ");
+            }
+            Console.Write("\n\n(User List Is Printing Only For Testing App)\nEnter user id:");
+            post.UserId = int.Parse(Console.ReadLine());
+
+            Thread repos = _threadRepo;
+            var threads = repos.GetThreads();
+            foreach (var thread in threads)
+            {
+                Console.Write($"\n{thread.Id}| {thread.Subject} ");
+            }
+            Console.Write("\nEnter thread id: ");
+            post.ThreadId = int.Parse(Console.ReadLine());
+
+            Console.Write("\nType Your Idea And Press Enter: ");
+            post.Content = Console.ReadLine();
+            _postRepo.Create(post);
+        }
+        public void UpdatePost(Post repo)
+        {
+            Console.WriteLine("\t------Update Post------\n");
+
+            Console.Write("Enter Old Post Id: ");
+            var postId = int.Parse(Console.ReadLine());
+            var post = repo.GetPostById(postId);
+
+            Console.Write("Type Your New Idea And Press Enter:: ");
+            post.Content = Console.ReadLine();
+            repo.Update(post);
+            Console.WriteLine("\nYour Post Updated. Press Any Key To Continue...");
+            Console.ReadKey();
+        }
+        public void DeletePost(Post repo)
+        {
+            Console.WriteLine("\t------Delete Post------\n");
+            Console.Write("Enter a Post Id: ");
+            var postId = int.Parse(Console.ReadLine());
+            var post = repo.GetPostById(postId);
+            repo.Delete(post);
+
+            Console.WriteLine("\nPost Deleted Successfully. Press Any Key To Continue...");
+            Console.ReadKey();
         }
     }
 }
